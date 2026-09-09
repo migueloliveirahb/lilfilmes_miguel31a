caef0e7044d384253d452fc41b61ae622ff422f0
migueloliveirahb <167984470+migueloliveirahb@users.noreply.github.com>
2026-09-09 07:42:22 -0300

    arrumando

--- frontend/cadastrar/cadastrar.js
+++ frontend/cadastrar/cadastrar.js
@@ -16,6 +16,8 @@ async function cadastrarFilme() {
         duracao: inputDuration.value
     }
 
+    console.log(filme)
+
     const informacoesAEnviar = {
         method: "POST",
         headers: {
@@ -24,7 +26,7 @@ async function cadastrarFilme() {
         body: JSON.stringify(filme)
     }
 
-    const resposta = await fetch("https://lilfilmes-miguel31a-goe4.vercel.app/add-movie", informacoesAEnviar)
+    const resposta = await fetch("http://localhost:8080/add-movie", informacoesAEnviar)
     const mensagemDecifrada = await resposta.json()
 
     alert(mensagemDecifrada.message)
